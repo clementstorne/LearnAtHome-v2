efea9ad63382188b34661b75e20465f7b179a7d7
Clément Storne <clementstorne@gmail.com>
2024-02-16 18:25:15 +0100

    API update single user route

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -1,5 +1,6 @@
 import { MISSING_PARAMETER, SERVER_ERROR } from "@/lib/errorMessages";
 import prisma from "@/lib/prisma";
+import bcrypt from "bcrypt";
 import { NextRequest } from "next/server";
 
 export async function GET(req: NextRequest) {
@@ -37,6 +38,82 @@ export async function GET(req: NextRequest) {
 
     return Response.json(
       { message: "Successfully got user.", user },
+      { status: 200 }
+    );
+  } catch (error) {
+    return Response.json(
+      {
+        message: SERVER_ERROR,
+        error,
+      },
+      { status: 500 }
+    );
+  }
+}
+
+type UpdateUserBody = {
+  name?: string;
+  email?: string;
+  password?: string;
+  imageUrl?: string;
+};
+
+export async function PATCH(req: NextRequest) {
+  const userId = req.nextUrl.pathname.split("/users/")[1];
+
+  if (!userId) {
+    return Response.json(
+      {
+        error: MISSING_PARAMETER,
+      },
+      { status: 400 }
+    );
+  }
+
+  try {
+    const { name, email, password, imageUrl }: UpdateUserBody =
+      await req.json();
+
+    const credentials: Partial<UpdateUserBody> = {};
+
+    if (name !== undefined) {
+      credentials.name = name;
+    }
+    if (email !== undefined) {
+      credentials.email = email;
+    }
+    if (password) {
+      const regex = /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W]).{8,})/g;
+      if (!regex.test(password)) {
+        Response.json(
+          {
+            error:
+              "The password provided does not meet the required format. Please ensure your password meets the specified criteria and try again.",
+          },
+          { status: 400 }
+        );
+      }
+      const hash = bcrypt.hashSync(password, 10);
+      credentials.password = hash;
+    }
+    if (imageUrl !== undefined) {
+      credentials.imageUrl = imageUrl;
+    }
+
+    const user = await prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: credentials,
+      select: {
+        name: true,
+        email: true,
+        role: true,
+      },
+    });
+
+    return Response.json(
+      { message: "Successfully updated user.", user },
       { status: 200 }
     );
   } catch (error) {
