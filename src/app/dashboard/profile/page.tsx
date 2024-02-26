3fe3882cddb1fb9e9260c39bdcb6aa8644b6de8b
Clément Storne <clementstorne@gmail.com>
2024-02-26 15:18:01 +0100

    profile form

--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -1,7 +1,16 @@
-const page = () => {
+import UserAvatar from "@/components/Avatar";
+import { getUser } from "@/lib/dataUser";
+import ProfileForm from "./components/ProfileForm";
+
+const userId = "b6566e5b-60d3-4e7e-8771-831e155d6c49";
+
+const page = async () => {
+  const user = await getUser(userId);
+
   return (
-    <main className="w-full h-full pt-48 px-8 md:pl-36 md:pr-8 flex">
-      <h2>Profil</h2>
+    <main className="w-full h-full pt-48 px-8 md:pl-36 md:pr-8 flex flex-col items-center">
+      <UserAvatar name={user.name} src={user.imageUrl} className="h-48 w-48" />
+      <ProfileForm user={user} />
     </main>
   );
 };
