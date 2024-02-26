8ffad02e0361d54ac9cbff49a7dec06c12bf3215
Clément Storne <clementstorne@gmail.com>
2024-02-26 15:19:06 +0100

    profile form

---
+++ src/app/dashboard/profile/components/ProfileForm.tsx
@@ -0,0 +1,184 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z
+  .object({
+    name: z.string().min(1, {
+      message: "Le nom est requis",
+    }),
+    email: z
+      .string()
+      .min(1, {
+        message: "L'email est requis",
+      })
+      .email({
+        message: "Cet email n'est pas valide",
+      }),
+    password: z
+      .string({
+        required_error: "Le mot de passe est requis",
+      })
+      .min(8, {
+        message: "Le mot de passe doit contenir au moins 8 caractères",
+      })
+      .refine(
+        (value) => /(?=.*\d)/.test(value),
+        "Le mot de passe doit contenir au moins un chiffre"
+      )
+      .refine(
+        (value) => /(?=.*[a-z])/.test(value),
+        "Le mot de passe doit contenir au moins une minuscule"
+      )
+      .refine(
+        (value) => /(?=.*[A-Z])/.test(value),
+        "Le mot de passe doit contenir au moins une majuscule"
+      )
+      .refine(
+        (value) => /(?=.*[\W|_])/.test(value),
+        "Le mot de passe doit contenir au moins un caractère spécial"
+      ),
+    confirmPassword: z
+      .string({
+        required_error: "Ce champ est requis",
+      })
+      .min(8, {
+        message: "Le mot de passe doit contenir au moins 8 caractères",
+      })
+      .refine(
+        (value) => /(?=.*\d)/.test(value),
+        "Le mot de passe doit contenir au moins un chiffre"
+      )
+      .refine(
+        (value) => /(?=.*[a-z])/.test(value),
+        "Le mot de passe doit contenir au moins une minuscule"
+      )
+      .refine(
+        (value) => /(?=.*[A-Z])/.test(value),
+        "Le mot de passe doit contenir au moins une majuscule"
+      )
+      .refine(
+        (value) => /(?=.*[\W|_])/.test(value),
+        "Le mot de passe doit contenir au moins un caractère spécial"
+      ),
+  })
+  .refine(
+    (values) => {
+      return values.password === values.confirmPassword;
+    },
+    {
+      message: "Les mots de passe ne sont pas identiques",
+      path: ["confirmPassword"],
+    }
+  );
+
+const ProfileForm = (userData: any) => {
+  const user = userData.user;
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: user.name,
+      email: user.email,
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="max-w-80 w-full mt-8 mx-auto space-y-8 flex flex-col items-center"
+      >
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="ml-2">Nom</FormLabel>
+              <FormControl>
+                <Input autoComplete="name" {...field} className="w-80" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="ml-2">Email</FormLabel>
+              <FormControl>
+                <Input
+                  type="email"
+                  autoComplete="email"
+                  {...field}
+                  className="w-80"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="ml-2">Mot de passe</FormLabel>
+              <FormControl>
+                <Input
+                  type="password"
+                  autoComplete="new-password"
+                  {...field}
+                  className="w-80"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="confirmPassword"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="ml-2">Confirmer le mot de passe</FormLabel>
+              <FormControl>
+                <Input
+                  type="password"
+                  autoComplete="new-password"
+                  {...field}
+                  className="w-80"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="w-60 !m-16">
+          Mondifier mon profil
+        </Button>
+      </form>
+    </Form>
+  );
+};
+
+export default ProfileForm;
